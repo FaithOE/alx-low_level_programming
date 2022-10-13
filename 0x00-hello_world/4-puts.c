1d497855d8425d3fa28ebb167fc62134eed6cd47
FaithOE <faithechobu@gmail.com>
2022-10-13 21:45:20 +0000

    Printing with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/* This C program uses puts instead of printf */
+
+/**
+* main - Entry point
+* Description: This C program uses puts to print instead of printf
+* Return: 0 if successful
+*/
 
 int main(void)
 {
