70081bab38f91e7e801b645d65bcf2d2ae3300cb
FaithOE <faithechobu@gmail.com>
2022-10-14 22:34:40 +0000

    Print alphabet in lowecase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* Description: A C program to identify last digit of a given number and compare
+* Return: 0 if successful
+*/
+
+int main(void)
+{
+	char lcase;
+
+	for (lcase = 'a'; lcase <= 'z'; lcase++)
+		putchar(lcase);
+		putchar('\n');
+	return (0);
+}
