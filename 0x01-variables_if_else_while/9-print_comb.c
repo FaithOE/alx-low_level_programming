62c3e5ed265d3d31b1670daa0c230b583f0b6b3f
FaithOE <faithechobu@gmail.com>
2022-10-15 01:04:19 +0000

    Number, comma, space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* Description: A C program to print single digit numbers with space and comma
+* Return: 0 if successful
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num + '0');
+		if (num < 9)
+		{
+		putchar(',');
+		putchar(" ");
+		}
+	}
+	putchar('\n');
+	return (0);
+}
