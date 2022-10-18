cdcf6deb98af3317a79f6239dfbc872aba24962e
FaithOE <faithechobu@gmail.com>
2022-10-18 16:40:50 +0000

    Use the print_alphabet function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+/**
+ * print_alphabet - Entry point
+ * Description: A C program to print the alphabet in lowercase
+ * Return: 0 if successful
+ */
+
+void print_alphabet(void)
+{
+	char lcase;
+
+	for (lcase = 'a'; lcase <= 'z'; lcase++)
+	{
+		putchar(lcase);
+	}
+	putchar('\n');
+	return (0);
+}
+
+/**
+  * main - Entry point
+  * Description: Use the function print_alphabet
+  * Return: 0 if successful
+  */
+
+int main(void)
+{
+	printf(print_alphabet);
+	return (0);
+}
