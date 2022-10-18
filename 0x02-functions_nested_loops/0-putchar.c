be1c449bb54c92c2e46861842609fdf3c6aff534
FaithOE <faithechobu@gmail.com>
2022-10-18 16:11:10 +0000

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Description: A C program to print _putchar
+  * Return: 0 if successful
+  */
+
+int main(void)
+{
+	printf("_putchar\n")
+	return (0)
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#Functions and nested loops
+Here are C programs that handle functions and nested loops
