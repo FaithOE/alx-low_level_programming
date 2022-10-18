f094e87fa3458e81e3a8cac46f1051371065a3dc
FaithOE <faithechobu@gmail.com>
2022-10-18 22:11:16 +0000

    Print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,8 +2,7 @@
 
 /**
  * print_alphabet - Entry point
- * Description: A C program to print the alphabet in lowercase
- * Return: 0 if successful
+ * Description: A C function to print the alphabet in lowercase
  */
 
 void print_alphabet(void)
@@ -15,5 +14,4 @@ void print_alphabet(void)
 		_putchar(lcase);
 	}
 	_putchar('\n');
-	return (0);
 }
