99c719ca6c6802132de789101f5a809ca04b763b
FaithOE <faithechobu@gmail.com>
2022-10-19 02:49:12 +0000

    Print from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,23 +10,22 @@
 
 void print_to_98(int n)
 {
-	if (n < 99)
-	{
-		for (; n < 99; n++)
-		{
-			printf("%d, ", n);
-		}
-	}
-	else if (n > 99)
-	{
-		for (; n > 99; n--)
-		{
-			printf("%d, ", n);
-		}
-	}
-	else if (n == 98)
+	if (n == 98)
 	{
 		printf("%d\n", n);
 	}
+	else if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n >= 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
 }
-
