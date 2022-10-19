5b3308f14698e8fa3deed5e8b5ff5923131e30d4
FaithOE <faithechobu@gmail.com>
2022-10-19 03:42:36 +0000

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -38,7 +38,7 @@ void print_times_table(int n)
 				else
 				{
 					_putchar(((product / 100) % 10) + '0');
-					_putchar(((product % 10) % 10) + '0');
+					_putchar(((product / 10) % 10) + '0');
 					_putchar((product % 10) + '0');
 				}
 			}
