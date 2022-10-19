9a4b185617f99b93c2dd397fb48e7c85bb9129b1
FaithOE <faithechobu@gmail.com>
2022-10-19 00:28:00 +0000

    What is the absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _abs - C function to compute the absolute value of an integer
+  *
+  *@n: number
+  *
+  * Return: the absolute value
+  */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * (-1);
+		return (n);
+	}
+
+}
