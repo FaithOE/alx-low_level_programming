dd90a27a6b1424a5ec2ff71321769a91f269395a
FaithOE <faithechobu@gmail.com>
2022-10-21 17:04:06 +0000

    Multiplication

---
+++ 0x04-more_functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * mul - C function to add two integers and return result
+  *
+  * @a: first integer
+  * @b: second integer
+  *
+  * Return: result
+  */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+
+	return (prod);
+}
