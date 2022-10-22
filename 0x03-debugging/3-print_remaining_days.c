d354e5bc4a38897d5d2070ff835daee8e7c8620c
FaithOE <faithechobu@gmail.com>
2022-10-22 00:21:47 +0000

    Remaining days in the year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -4,10 +4,12 @@
 /**
   * print_remaining_days - takes a date and prints how many days are
   * left in the year, taking leap years into account
+  *
   * @month: month in the number format
   * @day: day of month
   * @year: year
-  * Return: void
+  *
+  *
   */
 
 void print_remaining_days(int month, int day, int year)
