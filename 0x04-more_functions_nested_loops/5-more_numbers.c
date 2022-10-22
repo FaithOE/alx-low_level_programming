76ca0eaeef771881e285e11d00635ade0bad2cdb
FaithOE <faithechobu@gmail.com>
2022-10-22 02:30:11 +0000

    numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,17 +8,17 @@
 void more_numbers(void)
 {
 	int num;
-	char i;
+	int i;
 
 	for (num = 0; num <= 9; num++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
 			if (i > 9)
-			{
+			
 				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
-			}
+			
 		}
 		_putchar('\n');
 	}
