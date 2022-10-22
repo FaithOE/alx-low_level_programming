838deed0166a251a76ad80983c2fb955df7ef960
FaithOE <faithechobu@gmail.com>
2022-10-22 01:12:38 +0000

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,9 +12,9 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (a = '0'; a < size; a++)
+		for (a = 0; a < size; a++)
 		{
-			for (b = '0'; b < size; b++)
+			for (b = 0; b < size; b++)
 			{
 				_putchar('#');
 			}
@@ -23,6 +23,6 @@ void print_square(int size)
 	}
 	else
 	{
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
