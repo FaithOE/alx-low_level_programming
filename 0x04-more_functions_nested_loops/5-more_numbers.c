29d1dd7a55279c9b28a26b34107e832396d1a610
FaithOE <faithechobu@gmail.com>
2022-10-22 00:49:57 +0000

    print numbers 14 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,14 @@
 
 void more_numbers(void)
 {
-	char num;
+	int num;
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (num = '0'; num <= 14; num++)
+		for (num = 0; num <= 14; num++)
 		{
-			_putchar(num);
+			_putchar(num + '0');
 		}
 		_putchar('\n');
 	}
