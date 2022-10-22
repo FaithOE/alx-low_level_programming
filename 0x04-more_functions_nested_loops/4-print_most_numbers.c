1b17a0ce3ee1b2f1fd6db29b87f81e5bb99ef381
FaithOE <faithechobu@gmail.com>
2022-10-22 00:43:38 +0000

    Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,15 +7,15 @@
 
 void print_most_numbers(void)
 {
-	char num;
+	int num;
 
-	for (num = '0'; num <= '9'; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		if (num == 2 || num == 4)
 		{
 			continue;
 		}
-		_putchar(num);
+		_putchar(num + '0');
 	}
 	_putchar('\n')
 }
