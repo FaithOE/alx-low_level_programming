be0c4f884540babbae6d76409a9c7ecc7ade5a0a
FaithOE <faithechobu@gmail.com>
2022-10-25 01:19:51 +0000

    Copy from pointer to pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * _strcpy - A function that copies the string pointed to by src to the buffer
+  * pointed to by another pointer dest.
+  *
+  * @dest: Where the string is be copied to
+  * @src: Where the string is copied from
+  *
+  * Return: pointer to dest
+  */
+
+void *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	j = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[j] = src[i];
+		j++;
+	}
+	dest[j] = '\0';
+
+	return (dest);
+}
